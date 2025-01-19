9d7ebbcc743762f7d67a8b610166fbecb378b232
Davi Oliveira <davioliveira541@outlook.com>
2025-01-19 20:34:50 -0300

    Ligando leds
    Ligando os leds R, G e B, usando a comunicação serial

--- Leitura_Serial_Rotina.c
+++ Leitura_Serial_Rotina.c
@@ -3,11 +3,44 @@
 #include "pico/stdlib.h"
 #include "pico/bootrom.h"
 
+#define led_green 11
+#define led_blue 12
+#define led_red 13
+
 int main() {
-    stdio_init_all();
-    //Teste para comunicação serial
-    for( int i =0;i<10;i++){
-        printf("TESTE");
+    stdio_init_all(); 
+
+    gpio_init(led_green);
+    gpio_set_dir(led_green, GPIO_OUT);
+    gpio_init(led_blue);
+    gpio_set_dir(led_blue, GPIO_OUT);
+    gpio_init(led_red);
+    gpio_set_dir(led_red, GPIO_OUT);
+    gpio_init(buzzer);
+    gpio_set_dir(buzzer, GPIO_OUT);
+
+    char userInput;
+
+    while(1){
+        userInput = getchar();
+
+        if(userInput == '1'){
+            gpio_put(led_green, 1); 
+            gpio_put(led_blue, 0);
+            gpio_put(led_red, 0);
+            printf("LED verde ligado!\n");
+        }
+        else if(userInput == '2'){
+            gpio_put(led_green, 0);
+            gpio_put(led_blue, 1);
+            gpio_put(led_red, 0);
+            printf("LED azul ligado!\n");
+        }
+        else if(userInput == '3'){
+            gpio_put(led_green, 0);
+            gpio_put(led_blue, 0);
+            gpio_put(led_red, 1); 
+            printf("LED vermelho ligado!\n");
+        }
     }
-    reset_usb_boot(0, 0);//Função que reseta a placa no modo bootsel
 }
