b2abc21e437520d2fa7078414c7c80f09ec1466c
Amanda <amandaffangulo@gmail.com>
2025-01-19 21:46:18 -0300

    adição das teclas qwertyui para funcionamento do buzzer

--- Leitura_Serial_Rotina.c
+++ Leitura_Serial_Rotina.c
@@ -44,7 +44,40 @@ int main() {
             gpio_put(led_blue, 0);
             gpio_put(led_red, 1); 
             printf("LED vermelho ligado!\n");
-        }else if(userInput == 'x'){
+        }
+        else if(userInput == 'q'){
+            play_note(262, 100, buzzer); // C4
+            printf("Buzzer tocou C4!\n");
+        }
+        else if(userInput == 'w'){
+            play_note(294, 100, buzzer); // D4
+            printf("Buzzer tocou D4!\n");
+        }
+        else if(userInput == 'e'){
+            play_note(330, 100, buzzer); // E4
+            printf("Buzzer tocou E4!\n");
+        }
+        else if(userInput == 'r'){
+            play_note(349, 100, buzzer); // F4
+            printf("Buzzer tocou F4!\n");
+        }
+        else if(userInput == 't'){
+            play_note(392, 100, buzzer); // G4
+            printf("Buzzer tocou G4!\n");
+        }
+        else if(userInput == 'y'){
+            play_note(440, 100, buzzer); // A4
+            printf("Buzzer tocou A4!\n");
+        }
+        else if(userInput == 'u'){
+            play_note(494, 100, buzzer); // B4
+            printf("Buzzer tocou B4!\n");
+        }
+        else if(userInput == 'i'){
+            play_note(523, 100, buzzer); // C5
+            printf("Buzzer tocou C5!\n");
+        }
+        else if(userInput == 'x'){
             printf("Resetando a placa\n");
             reset_usb_boot(0, 0);
         }
